b7aeb5bd1d9a4fbf623ca12fa045b48658aa1379
Swedneck <git@swedneck.xyz>
2026-06-29 11:52:09 +0200

    only create build dir if it doesn't exist

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-mkdir build
+
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
 cd modpack
+
 version=$(cat modinfo.json | python -c "import json,sys; obj=json.load(sys.stdin); print(obj['version']);")
+
 zip -r "../build/vsexpanded-${version}.zip" ./*
