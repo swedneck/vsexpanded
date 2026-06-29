d87028278345f7523aa9d705889b0a0ce44234d0
Swedneck <git@swedneck.xyz>
2026-06-29 13:20:59 +0200

    even better build script?? should handle invalid json better now and properly warn about it

--- build.sh
+++ build.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
 
+#only create build folder if it doesn't already exist
 if [ ! -d "build" ]; then
   mkdir build
 fi
 
-cd modpack
+# only continue if valid, otherwise error
+if python -mjson.tool "modpack/modinfo.json" > /dev/null 2> /dev/null; then
 
-version=$(cat modinfo.json | python -c "import json,sys; obj=json.load(sys.stdin); print(obj['version']);")
+  version=$(cat modpack/modinfo.json | python -c "import json,sys; obj=json.load(sys.stdin); print(obj['version']);")
+  #horrid hack to avoid a wrapping folder inside the zip
+  cd modpack
+  zip -r "../build/vsexpanded-${version}.zip" ./*
+  cd ..
+  exit 0
 
-zip -r "../build/vsexpanded-${version}.zip" ./*
+else
+
+  validation=$(python -mjson.tool "modpack/modinfo.json" 2>&1)
+  date=$(date '+%Y-%m-%dT%H:%M:%S')
+  errorfile="build/error_${date}.log"
+  echo "ERROR: Failed to validate modinfo.json" | tee -a $errorfile
+  printf "\n" >> $errorfile
+  echo $validation | tee -a $errorfile
+  exit 1
+
+fi
